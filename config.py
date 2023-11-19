1883a77e306b31ade6d9dc5b9a5bd4019ced46d3
Mangesh Yadav <106221121+Mangesh-Yadav12@users.noreply.github.com>
2023-11-19 08:51:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from os import environ
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'Mangesh@12'
+
+    UPLOADS = "Users/91952/PycharmProjects/Text_Extraction_from_Image/app/static/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class DebugConfig(Config):
+
+    DEBUG = False
